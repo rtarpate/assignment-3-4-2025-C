a8e46f3b6eb35f2064c5a6e3e88a1c49ad9bd949
Ashton Pate <arpate@amarillocollege.com>
2025-03-04 13:44:01 -0600

    Add project files.

---
+++ assignment 3 4 2025 C/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_3_4_2025_C
+{
+    internal class Program
+    {
+        static Dictionary<string, int> votes = new Dictionary<string, int>();
+        static void Main(string[] args)
+        {
+            string can1 = "Trump";
+
+            string can2 = "Kamala";
+
+            string can3 = "Kenny";
+
+            int choice = 0;
+
+           
+
+            votes.Add(can1, 0);
+            votes.Add(can2, 0);
+            votes.Add(can3, 0);
+
+            while (true)
+            {
+                Console.WriteLine($" who do you vote for 1 for {can1} 2 for {can2} 3 for {can3} 4 for reset votes");
+
+                choice =Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        CastVoted(can1);
+                        break;
+                    case 2:
+                        CastVoted(can2);
+                        break;
+                    case 3:
+                        CastVoted(can3);
+                        break;
+                    case 4:
+                        ResetVotes();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        static void CastVoted(string candidateName)
+        {
+            votes[candidateName]++;
+
+            DisplayResults();
+
+            Console.WriteLine();
+
+        }
+
+        static void DisplayResults()
+        {
+            foreach (KeyValuePair<string, int> vote in votes)
+            {
+                Console.WriteLine(vote.Key + " " + vote.Value);
+            }
+            Console.WriteLine();
+
+        }
+
+        static void ResetVotes()
+        {
+            Dictionary<string, int> newvotes = new Dictionary<string, int>();
+
+            foreach (KeyValuePair<string, int> vote in newvotes)
+            {
+                newvotes.Add(vote.Key, 0);
+            }
+
+            votes = newvotes;
+
+            DisplayResults();
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ assignment 3 4 2025 C/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("assignment 3 4 2025 C")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("assignment 3 4 2025 C")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("085664f3-b939-40c5-9286-7ad7e34d2458")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
